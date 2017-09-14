c17a7750ec6adede91a5a0a26d1b80dd02eea6f2
guowei0702 <guowei0702@hotmail.com>
2017-09-14 10:35:34 +0000

    v7

--- src/index.php
+++ src/index.php
@@ -22,6 +22,8 @@
                 <p>This is new version 3.</p>
                 <p>This is new version 4.</p>
                 <p>This is new version 5.</p>
+                <p>This is new version 6.</p>
+                <p>This is new version 7.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
