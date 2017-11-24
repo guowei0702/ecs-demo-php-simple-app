030639371d8758fea420f42d1941fc6807248a31
root <root@ip-172-31-2-131.cn-north-1.compute.internal>
2017-11-24 13:05:50 +0000

    back2v3

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>This is new version 1.</p>
                 <p>This is new version 2.</p>
+                <p>This is new version 3.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
