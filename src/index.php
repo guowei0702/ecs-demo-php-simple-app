fffcc874eb31bd5e7a2c870366d16c8585680272
root <root@ip-172-31-12-20.cn-north-1.compute.internal>
2018-07-06 05:01:49 +0000

    j-v3

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
                 <h1 style="color:orange;">Amazon ECS Awesome!</h1>
                 <h1 style="color:orange;">Jenkins is good!</h1>
                 <h1 style="color:orange;">Jenkins is good!</h1>
+                <h1 style="color:orange;">Jenkins is good!</h1>
                 <h2>Congratulations</h2>
                 <p>This is new version 2.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
