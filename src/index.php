3fa4c9cbd212992bc8ee5cba30a503bb44d03ff4
Guo <gowe@186590cc5201.ant.amazon.com>
2018-09-20 10:30:11 +0800

    v2-0920

--- src/index.php
+++ src/index.php
@@ -17,9 +17,9 @@
             <div class="hero-unit">
                 <h1 style="color:orange;">Hello AWS World!</h1>
                 <h1 style="color:orange;">Amazon ECS Awesome!</h1>
-                <h1 style="color:orange;">Jenkins is good!</h1>
+                <h1 style="color:orange;">Jenkins is awsome!</h1>
                 <h2>Congratulations</h2>
-                <p>This is new version 3.</p>
+                <p>This is new version 1.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
